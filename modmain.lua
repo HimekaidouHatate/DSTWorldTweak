24e0048737a5611ea0c95be777ecdd33049d38c8
HimekaidouHatate <yys14239102@gmail.com>
2021-05-09 20:51:06 +0900

    named.lua
    Global Test에 넣을껄

--- modmain.lua
+++ modmain.lua
@@ -40,7 +40,6 @@ local function CaveEntranceFn(inst)
 end
 AddPrefabPostInit("cave_entrance_open", CaveEntranceFn)
 
-
 local function CanTakeItemTweak(inst)
 	local _old = inst.components.container.CanTakeItemInSlot
 	inst.components.container.CanTakeItemInSlot = function(self, item, slot)
@@ -85,4 +84,70 @@ local function UpdateBackpack(inst)
 	inst:ListenForEvent("itemlose", CheckStatus)
 end
 
+local dirtystr = ""
+local function CleanStr(inst)
+	if dirtystr == "" then return end
+
+	inst.debugstring:set(dirtystr)
+	dirtystr = ""
+end
+
+local function QueueStr(str)
+	dirtystr = dirtystr.."\n"..str
+end
+
+local function DebugCustom(...)
+	local str = ""
+    local n = GLOBAL.select('#', ...)
+    local args = GLOBAL.toarray(...)
+    for i = 1, n do
+        str = str..tostring(args[i]).."\t"
+    end
+
+	GLOBAL.AllPlayers[1].components.talker:Say(str)
+	if not (GLOBAL.TheNet:GetIsClient() and (GLOBAL.TheNet:GetIsServerAdmin() or GLOBAL.IsConsole())) then
+		QueueStr(str)
+	end
+end
+
+local function PrintOnLocal(inst)
+	if inst._parent.HUD == nil then return end
+	local str = inst.debugstring:value()
+	print(str)
+end
+
+local function RegisterModNetListeners(inst)
+	if GLOBAL.TheWorld and GLOBAL.TheWorld.ismastersim then
+		inst._parent = inst.entity:GetParent()
+	end
+
+	--inst:ListenForEvent("debugstringdirty", PrintOnLocal)
+	--inst:DoPeriodicTask(0, CleanStr)
+	--GLOBAL.AddPrintLogger(DebugCustom)
+end
+
+AddPrefabPostInit("player_classified", function(inst)
+	inst.debugstring = GLOBAL.net_string(inst.GUID, "debugstring", "debugstringdirty")
+
+	inst:DoTaskInTime(2 * GLOBAL.FRAMES, RegisterModNetListeners) 
+	-- delay two more FRAMES to ensure the original NetListeners to run first.
+end)
+
 AddPrefabPostInit("backpack", UpdateBackpack)
+
+AddComponentPostInit("named", function(self)
+    local function DoSetName(self) -- UpvalueHacker를 사용하지 않고 기존 함수 덮어씀
+        self.inst.name = self.nameformat ~= nil and string.format(self.nameformat, self.name) or self.name
+        self.inst.replica.named:SetName(self.inst.name)
+    end
+    
+    self.SetName = function(self, name)
+        self.name = "Yukari"
+        if name == nil then
+            self.inst.name = GLOBAL.STRINGS.NAMES[string.upper(self.inst.prefab)]
+            self.inst.replica.named:SetName("")
+        else
+            DoSetName(self)
+        end
+    end
+end)
